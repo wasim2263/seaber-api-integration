4d415cc057220274e9459e8bb7f7fd910c772a20
abdullah.wasim <abdullahwasim42@gmail.com>
2022-04-25 23:10:26 +0600

    base project setup

---
+++ src/app/swagger/constants.ts
@@ -0,0 +1,4 @@
+export const SWAGGER_API_ROOT = 'api/api-docs';
+export const SWAGGER_API_NAME = 'QUIZ API Doc';
+export const SWAGGER_API_DESCRIPTION = 'QUIZ Api Description';
+export const SWAGGER_API_CURRENT_VERSION = '1.0';

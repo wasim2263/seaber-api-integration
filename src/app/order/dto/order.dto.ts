93de58746b619e0d54f3af72b478ce660df8216c
abdullah.wasim <abdullahwasim42@gmail.com>
2022-04-25 23:38:17 +0600

    modified module configuration

--- src/app/order/dto/order.dto.ts
+++ src/app/order/dto/order.dto.ts
@@ -1,2 +1,39 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {IsEnum, IsNumber, IsOptional, IsString, IsUUID} from 'class-validator';
+
+
+export enum OrderType {
+    FROM = 'from',
+    TO = 'to',
+    CARGO = 'cargo',
+}
 export class OrderDto {
+    @ApiProperty()
+    @IsUUID()
+    readonly extOrderId: string;
+
+    @ApiProperty()
+    @IsEnum(OrderType)
+    readonly type: OrderType;
+
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    readonly fromLocation?: string;
+
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    readonly toLocation?: string;
+
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    readonly cargoType?: string;
+
+    @ApiPropertyOptional()
+    @IsNumber()
+    @IsOptional()
+    readonly cargoAmount?: number;
+
 }
